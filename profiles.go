4cb8d2783677b483ec1e92e6a06b8f4b54ac5e37
DCH81 <azulax9@gmail.com>
2024-10-29 17:20:27 +0100

    Shuffle ciphers

--- profiles.go
+++ profiles.go
@@ -2,6 +2,7 @@ package azuretls
 
 import (
 	"math/rand"
+	"time"
 
 	"github.com/Noooste/fhttp/http2"
 	tls "github.com/Noooste/utls"
@@ -42,6 +43,18 @@ func getShuffledExtensions(extensions []tls.TLSExtension) []tls.TLSExtension {
 	final = append(final, &tls.UtlsPaddingExtension{GetPaddingLen: tls.BoringPaddingStyle})
 
 	return final
+}
+
+func getShuffledCiphers(ciphers []uint16) []uint16 {
+	shuffled := make([]uint16, len(ciphers))
+	copy(shuffled, ciphers)
+
+	rand.Seed(time.Now().UnixNano()) // Seed the random number generator
+	rand.Shuffle(len(shuffled), func(i, j int) {
+		shuffled[i], shuffled[j] = shuffled[j], shuffled[i]
+	})
+
+	return shuffled
 }
 
 // GetLastChromeVersion apply the latest Chrome version
@@ -101,25 +114,27 @@ func GetLastChromeVersion() *tls.ClientHelloSpec {
 		// &tls.UtlsPaddingExtension{GetPaddingLen: tls.BoringPaddingStyle},
 	}
 
+	ciphers := []uint16{
+		tls.GREASE_PLACEHOLDER,
+		tls.TLS_AES_128_GCM_SHA256,
+		tls.TLS_AES_256_GCM_SHA384,
+		tls.TLS_CHACHA20_POLY1305_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+		tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+		tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+		tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+	}
+
 	return &tls.ClientHelloSpec{
-		CipherSuites: []uint16{
-			tls.GREASE_PLACEHOLDER,
-			tls.TLS_AES_128_GCM_SHA256,
-			tls.TLS_AES_256_GCM_SHA384,
-			tls.TLS_CHACHA20_POLY1305_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-		},
+		CipherSuites: getShuffledCiphers(ciphers),
 		CompressionMethods: []byte{
 			0x00, // compressionNone
 		},
